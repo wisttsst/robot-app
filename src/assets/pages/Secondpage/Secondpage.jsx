1c4df68c12b4a0c6e6e2b9e82ffae691c50d392b
PC26 <rbaco25a-0030@olopsc.edu.ph>
2026-02-05 15:08:27 +0800

    Update fix button with inline SVG icon

--- src/assets/pages/Secondpage/Secondpage.jsx
+++ src/assets/pages/Secondpage/Secondpage.jsx
@@ -1,7 +1,6 @@
 import { useParams, useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import fixButtonIcon from '../../fix-icon.png'
 import { getRobotByName, addRobotToFirestore, updateRobotInFirestore } from '../../../firebase'
 import './Secondpage.css'
 
@@ -140,7 +139,9 @@ function Secondpage () {
                         </div>
                         <p>{fillPercentage}%</p>
                         <button className='button-primary fix-button' onClick={handleFillButton} disabled={fillPercentage === 100}>
-                            <img src={fixButtonIcon} alt="Fix" className='fix-button-icon' />
+                            <svg className='fix-button-icon' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2'>
+                                <path d='M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 1 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z'></path>
+                            </svg>
                             <span>{fillPercentage === 100 ? 'Ready to Name' : 'Repair'}</span>
                         </button>
 
